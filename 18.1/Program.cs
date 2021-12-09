2982cd581b87b7e661bd13634ce2f6561e43bf87
Sveta <Sveta@KOMP>
2021-12-09 21:09:50 +0300

    Добавьте файлы проекта.

---
+++ 18.1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            var sk = new Dictionary<char, char>
+           {
+              {')','('},
+               {'}','{'},
+               {']','['},
+           };
+            var brackets = new Stack<char>();
+            foreach (var sko in str)
+            {
+                if (sko == '[' || sko == '(' || sko == '{')
+                    brackets.Push(sko);
+
+
+                if (sko == ']' || sko == ')' || sko == '}')
+                {
+
+                    if (brackets.Count == 0)
+                    {
+                        brackets.Push(sko);
+                        break;
+                    }
+                    if (brackets.Pop() != sk[sko])
+                    {
+                        break;
+                    }
+                }
+
+            }
+
+            if (brackets.Count == 0)
+            {
+                Console.WriteLine("Скобки расставлены правильно");
+            }
+            else
+            {
+                Console.WriteLine("Скобки расставлены не правильно");
+            }
+            Console.ReadKey();
+
+
+
+        }
+
+    }
+}
